1691c7cbaa2acc420f1cc0ffc9a06f4cfcafe224
docente2 <giuseppe.monciino@branchina.edu.it>
2025-07-02 11:38:47 +0200

    Inserito input utente nel main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        InputStreamReader reader = new InputStreamReader(System.in);
+        BufferedReader in = new BufferedReader(reader);
 
+        try {
+            String input = in.readLine();
+        }catch (Exception e){
+            System.out.println(e);
+
+        }
 
     }
 }
